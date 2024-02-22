95e66b635ebe8680dd8df346fad640290dc8ff1d
ajaytitus1386 <ajaytitus1386@gmail.com>
2024-02-22 15:10:01 +0530

    commenting redirects

--- next.config.js
+++ next.config.js
@@ -2,21 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async redirects() {
-    return [
-      {
-        source: "/(^.+)",
-        has: [
-          {
-            type: "host",
-            value: "wells.hopto.org"
-          },
-        ],
-        destination: "https://wells.hopto.org/",
-        permanent: true,
-      },
-    ]
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/(^.+)",
+  //       has: [
+  //         {
+  //           type: "host",
+  //           value: "wells.hopto.org"
+  //         },
+  //       ],
+  //       destination: "https://wells.hopto.org/",
+  //       permanent: true,
+  //     },
+  //   ]
+  // },
   async rewrites() {
     return {
       beforeFiles: [
