e2babde91abe29afa26ebadd650103ebe9926df1
ajaytitus1386 <ajaytitus1386@gmail.com>
2024-02-22 11:10:10 +0530

    remove afterFiles object key

--- next.config.js
+++ next.config.js
@@ -3,19 +3,32 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   async rewrites() {
-    return {
-      afterFiles: [
-        {
-          source: "/",
-          destination: "/asimov",
-        },
-        {
-          source: "/asimov",
-          destination: "/asimov",
-        },
+    return [
+      {
+        source: "/",
+        has: [
+          {
+            type: "host",
+            value: "wells.hopto.org"
+          },
+        ],
+        destination: "/asimov",
+      }
+    ]
+
+    // return {
+    //   afterFiles: [
+    //     {
+    //       source: "/",
+    //       destination: "/asimov",
+    //     },
+    //     {
+    //       source: "/asimov",
+    //       destination: "/asimov",
+    //     },
         
-      ]
-    }
+    //   ]
+    // }
     }
 }
 
