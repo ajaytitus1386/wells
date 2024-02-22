d47dd7bfc2af1b6907811c35730ea787d0589544
ajaytitus1386 <ajaytitus1386@gmail.com>
2024-02-22 18:22:17 +0530

    fix: regex syntax

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: "/^(?!asimov$).+",
+        source: "/(^(?!asimov$).+)",
         has: [
           {
             type: "host",
